4691199822a375f761b53dae63f946b5a34e5acc
Artem Zholus <artem.zholus@gmail.com>
2022-07-28 14:12:54 +0000

    bugfix

--- sample_factory/algorithms/dummy_sampler/sampler.py
+++ sample_factory/algorithms/dummy_sampler/sampler.py
@@ -76,7 +76,7 @@ class DummySampler(AlgorithmBase):
         # creating an environment in the main process tends to fix some very weird issues further down the line
         # https://stackoverflow.com/questions/60963839/importing-opencv-after-importing-pytorch-messes-with-cpu-affinity
         # do not delete this unless you know what you're doing
-        tmp_env = create_env(self.cfg.env, cfg=self.cfg, env_config=None)
+        tmp_env = create_env(self.cfg.env, cfg=self.cfg, env_config={'fake': True})
         tmp_env.close()
 
         for i in range(self.cfg.num_workers):
